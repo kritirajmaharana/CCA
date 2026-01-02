f071410ab3d0eb0d8b30ef336f979dc89f46980e
Kritiraj Maharana <12345678+kritirajmaharana@users.noreply.github.com>
2026-01-02 22:43:39 +0530

    EDA: initial exploration and null analysis

--- analysis.py
+++ analysis.py
@@ -1,8 +1,9 @@
 import pandas as pd 
 
 df = pd.read_csv("Churn_Modelling.csv")
-# print(df.head())
-# print(df.shape)
-# print(df.info())
-# print(df.describe())
+print(df.head())
+print(df.shape)
+print(df.info())
+print(df.describe())
 print(df.isnull().sum())
+print("hello world")
